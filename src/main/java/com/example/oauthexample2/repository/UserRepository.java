c5969d156a2b34b7cac2e2e0321e425a88471368
Jovana Zivkovic <116586798+jovanazivkovic99@users.noreply.github.com>
2023-05-16 11:43:53 +0200

    Added UserRepository

---
+++ src/main/java/com/example/oauthexample2/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.oauthexample2.repository;
+
+import com.example.oauthexample2.document.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
